58ffdf2bcc85144246650c2f050b945c6bde1287
akanniquadree <akanniquadry7@gmail.com>
2024-09-06 15:14:44 +0100

    Changes Updated 3

--- Routes/sponsorRoute/sponsorRoute.js
+++ Routes/sponsorRoute/sponsorRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cloudinary = require("cloudinary");
 const dotenv = require("dotenv");
-const SponsorModel = require("../../Model/SponsorModel/SponsorModel");
+const SponsorModel = require("../../Model/SponsorModel/SponsorModel.js");
 
 dotenv.config();
 cloudinary.config({
